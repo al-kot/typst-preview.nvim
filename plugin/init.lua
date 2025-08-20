9d3c404b3b909fe1144c7f7bf504787afddf7fd5
Aleksei <aleksei.kotliarov@epita.fr>
2025-08-20 03:42:01 +0200

    feat: add goto command

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,6 +1,12 @@
 vim.api.nvim_create_user_command("TypstPreviewStart", function()
     require("typst-preview").start()
 end, {})
+
 vim.api.nvim_create_user_command("TypstPreviewStop", function()
     require("typst-preview").stop()
 end, {})
+
+vim.api.nvim_create_user_command("TypstPreviewGoTo", function(opts)
+    local n = tonumber(opts.args)
+    if n then require("typst-preview").goto_page(n) end
+end, { nargs = 1 })
